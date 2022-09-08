fcba42ceefa716f1feea84137fbf191240ddc3d4
Monde Nyawose <lionelnyawose@gmail.com>
2022-09-08 07:42:41 +0200

    addded animejs

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+
+window.onload = function(){
+  animateLoad()
+}
+
+
 const options = {
   navigation: {
     nextEl: '.swiper-button-next',
@@ -21,4 +27,53 @@ const options = {
 const workSwiper = new Swiper('.corporate', options);
 const projectSwiper = new Swiper('.projects', options);
 const expSwiper = new Swiper('.experiments', options);
+
+function animateLoad(){
+
+  const heading = document.querySelector('.header h1')
+  const text = document.querySelectorAll('.header p')
+  const bgImg = document.querySelector('.bg img')
+  const cover = document.querySelector('.bgCover')
+  const loader = document.querySelector('.loader')
+  const initAnimations = {
+    duration:500,
+    easing:'linear',
+
+  }
+  const loadTL = anime.timeline(initAnimations)
+
+  loadTL
+  .add({
+    targets:loader,
+    // translateX:['0','100%'],
+    opacity:[1,0],
+    duration:1000,
+    delay:1000
+  })
+    .add({
+    targets:loader,
+    translateX:['0','100%'],
+  })
+  .add({
+    targets:cover,
+    translateY:['0%','-100%'],
+    duration:300
+  })
+  .add({
+    targets:bgImg,
+    scale:['110%','100%'],
+    duration:1000,
+    easing: 'linear'
+  })
+  .add({
+    targets: [heading,text],
+    delay:100,
+    opacity:[0,1],
+    translateY:['10px','0px']
+  })
+
+ 
+}
+
+
 
