54de1f9444e3cd6be20d8325459ca5ec46274182
Monde Nyawose <lionelnyawose@gmail.com>
2022-09-28 00:17:12 +0200

    [FEATURE] add locomotive scroll

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 window.addEventListener('load', function(){
    animateLoad()
+
 })
+
+const scroll = new LocomotiveScroll({
+   el: document.querySelector('[data-scroll-container]'),
+  smooth: true
+});
 
 
 const options = {
